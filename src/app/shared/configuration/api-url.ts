53549b9336faada9e880e339e0f402cad397a430
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-22 07:15:24 -0600

    add URL to call api un market rates service

---
+++ src/app/shared/configuration/api-url.ts
@@ -0,0 +1,7 @@
+import { environment } from "src/environments/environment";
+
+
+export const API_URLS = {
+    BASE_URL: `${environment.GBM_URL_API}/${environment.GBM_API_VERSION}/`,
+    MARKET_RATES: 'cc4c350b-1f11-42a0-a1aa-f8593eafeb1e'
+}
