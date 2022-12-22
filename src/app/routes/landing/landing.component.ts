e3fcfd4d824a3569b81580af85616bc3a123323c
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-22 07:13:19 -0600

    call service getMarket Rates

--- src/app/routes/landing/landing.component.ts
+++ src/app/routes/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { EChartsOption } from 'echarts/types/dist/echarts';
-import * as moment from 'moment';
-
+import { TranslateService } from '@ngx-translate/core';
+import { take } from 'rxjs';
+import { PriceMarketRatesService } from 'src/app/shared/services/price-market-rates.service';
 @Component({
   selector: 'GBM-landing',
   templateUrl: './landing.component.html',
@@ -11,95 +11,44 @@ export class LandingComponent implements OnInit {
   options: any;
   updateOptions: any;
 
-  private oneDay = 24 * 3600 * 1000;
-  private now: Date = new Date();
-  private value: number = 0;
-  private data: any[] = [];
-  private timer: any;
-  constructor() {
+  public rateList: any = null;
+  public loadingPrices = true;
+  public errorService = false;
 
-  }
+  language: string = 'en';
+
+  constructor(
+    private _priceMarketRatesService: PriceMarketRatesService,
+    private _translateService: TranslateService
+  ) { }
 
   ngOnInit(): void {
+    this._getMarketRates();
+  }
 
-
-    this.data = [];
-    this.now = new Date(1997, 9, 3);
-    this.value = Math.random() * 1000;
-
-    for (let i = 0; i < 1000; i++) {
-      this.data.push(this.randomData());
-    }
-
-    // initialize chart options:
-    this.options = {
-      title: {
-        text: 'Dynamic Data + Time Axis'
+  private _getMarketRates() {
+    this.loadingPrices = true;
+    this._priceMarketRatesService.getIndexMarketRates().pipe(take(1)).subscribe({
+      next: (resp) => {
+        this.rateList = resp;
       },
-      tooltip: {
-        trigger: 'axis',
-        formatter: (params: any) => {
-          params = params[0];
-          const date = new Date(params.name);
-          return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-        },
-        axisPointer: {
-          animation: false
-        }
+      error: (error) => {
+        this.errorService = true;
+        console.error(error);
       },
-      xAxis: {
-        type: 'time',
-        splitLine: {
-          show: false
-        }
-      },
-      yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-          show: false
-        }
-      },
-      series: [{
-        name: 'Mocking Data',
-        type: 'line',
-        showSymbol: false,
-        emphasis: {
-          line: false,
-        },
-        data: this.data
-      }]
-    };
-
-    // Mock dynamic data:
-    this.timer = setInterval(() => {
-      for (let i = 0; i < 5; i++) {
-        this.data.shift();
-        this.data.push(this.randomData());
+      complete: () => {
+        this.loadingPrices = false;
       }
-
-      // update series data:
-      this.updateOptions = {
-        series: [{
-          data: this.data
-        }]
-      };
-    }, 1000);
+    });
   }
 
-  ngOnDestroy() {
-    clearInterval(this.timer);
-  }
+  public onSelect(lang: any): void {
 
-  randomData() {
-    this.now = new Date(this.now.getTime() + this.oneDay);
-    this.value = this.value + Math.random() * 21 - 10;
-    return {
-      name: this.now.toString(),
-      value: [
-        [this.now.getFullYear(), this.now.getMonth() + 1, this.now.getDate()].join('/'),
-        Math.round(this.value)
-      ]
-    };
+    console.log(lang);
+
+    localStorage.setItem('language', lang);
+    this._translateService.use(lang);
+
+
   }
 }
