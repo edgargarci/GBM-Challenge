b4786b94866cbee7088426047297a274aafd2b7e
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-23 16:41:05 -0600

    configure useer name in menubar

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { CONSTANTS } from '../../utils/constants';
 
 @Component({
   selector: 'app-menu-bar',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
-  constructor() { }
+  @Input() user: string = "";
+  public nick: string = '';
 
+  constructor(public translate: TranslateService) {
+    translate.addLangs(CONSTANTS.LANGS_LIST);
+    translate.setDefaultLang(CONSTANTS.DEFAULT_LANG);
+  }
   ngOnInit(): void {
+    this._generateNick(this.user)
+  }
+
+  private _generateNick(name: string) {
+    const nameFull = name.split(CONSTANTS.REG_EXP.SPACE);
+    const [firstName, secondName] = nameFull;
+    this.nick = secondName ? `${firstName.charAt(0)}${secondName.charAt(0)}` : `${firstName.charAt(0)}`
   }
 
 }
