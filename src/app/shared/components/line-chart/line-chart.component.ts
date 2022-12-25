4858c1a00672083803424956275cffc073cc16a0
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-25 16:53:43 -0600

    refactor line chart component

--- src/app/shared/components/line-chart/line-chart.component.ts
+++ src/app/shared/components/line-chart/line-chart.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import { interval } from 'rxjs';
+import { IMarketRateModule } from '../../models/market-rates.model';
+import { CONSTANTS } from '../../utils/constants';
+import { GenerateLineChartModule } from './generaLineChart.model';
 
 @Component({
   selector: 'GBM-line-chart',
@@ -10,50 +14,64 @@ export class LineChartComponent implements OnInit {
 
   @Input() data: any = [{}];
 
-  options: any;
+  options: any = {};
   updateOptions: any;
+  public marketsIndex: Array<IMarketRateModule> = [];
+  public timeNewValue$: any = interval(1000);
+  public last = 39_000;
 
   ngOnInit(): void {
-    console.log(this.data);
+    this.options = new GenerateLineChartModule({}, this.marketsIndex).options;
+    this._generateNewValue();
+  }
+  private _generateNewValue() {
 
-    let data1: any = [];
-    this.data.forEach((item: any) => {
-      data1.push({
-        name: item.date,
-        value: [item.date, item.price],
-        fullDate: moment(item.date)
-      })
+    this.timeNewValue$.subscribe({
+      next: () => {
+        this._generateRandomValue();
+        this._updateData();
+      }
+    });
+  }
+
+  private _generateRandomValue() {
+    let minutesChart = this._generateDate(this.marketsIndex.length, CONSTANTS.TIME_VALUES.MINUTES);
+    const sum = this._random(2);
+    const value = this._random(2000);
+
+    this.last = sum ? (this.last + value) : (this.last - value);
+    minutesChart += 60;
+    const day = this._generateDate(minutesChart, CONSTANTS.TIME_VALUES.SECONDS);
+
+    this.marketsIndex.length > 50 ?? this.marketsIndex.shift();
+
+    this.last = this.last + this._random(500) - 100;
+
+    this.marketsIndex.push({
+      name: day,
+      value: [day, this.last],
     });
 
-    this.options = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          animation: false
-        }
-      },
-      xAxis: {
-        type: 'time',
-        splitLine: {
-          show: false
-        }
-      },
-      yAxis: {
-        type: 'value',
-        min: 39000,
-        boundaryGap: [0, '100%'],
-        splitLine: {
-          show: false
-        }
-      },
+  }
+
+  private _generateDate(value: any, typeTime: string) {
+    return (moment().add(value, typeTime)).format();
+  }
+
+  private _random(factor: number) {
+    return Math.floor(Math.random() * factor)
+  }
+
+  private _updateData() {
+    this.updateOptions = {
       series: [{
-        type: 'line',
-        showSymbol: false,
-        emphasis: {
-          line: false,
-        },
-        data: data1
+        data: this.marketsIndex
       }]
     };
   }
+
+  ngOnDestroy(): void {
+    this.timeNewValue$.unsubscribe();
+  }
+
 }
