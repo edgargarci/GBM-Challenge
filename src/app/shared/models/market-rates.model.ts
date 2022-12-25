6fa1c7b967e6ef574c45a4e2c026cb18d45a975f
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-25 16:38:24 -0600

    optional values in model, defaults values in zero

--- src/app/shared/models/market-rates.model.ts
+++ src/app/shared/models/market-rates.model.ts
@@ -1,11 +1,11 @@
 import * as moment from "moment";
 
 export interface IMarketRateModule {
-    date: moment.Moment | string;
-    price: number;
-    percentageChange: number;
-    volume: number;
-    change: number;
+    date?: moment.Moment | string;
+    price?: number;
+    percentageChange?: number;
+    volume?: number;
+    change?: number;
     value?: [string, number];
     name?: string
 }
@@ -21,11 +21,11 @@ export class MarketRateModule implements IMarketRateModule {
 
     constructor(item: IMarketRateModule) {
         this.date = moment(item.date).format('LTS');
-        this.price = item.price;
-        this.percentageChange = item.percentageChange;
-        this.volume = item.volume;
-        this.change = item.change;
-        this.value = [this.date, item.price];
+        this.price = item.price ?? 0;
+        this.percentageChange = item.percentageChange ?? 0;;
+        this.volume = item.volume ?? 0;;
+        this.change = item.change ?? 0;;
+        this.value = [this.date, item.price ?? 0];
         this.name = this.date
     }
 }
