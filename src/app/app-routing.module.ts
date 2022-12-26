8c671be7ec8c7baa001c5b9e8e588a884284f4e7
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-26 01:42:50 -0600

    configurate routes login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './routes/landing/landing.component';
+import { LoginComponent } from './routes/login/login.component';
 
 const routes: Routes = [{
   path: '',
+  redirectTo: 'login',
+  pathMatch: 'full'
+},
+{
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'landing',
   component: LandingComponent
-}];
+},
+{
+  path: '**',
+  component: LandingComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
