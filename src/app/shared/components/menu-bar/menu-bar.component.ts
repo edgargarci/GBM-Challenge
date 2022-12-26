3d8e325b6581edb366253330c2ff68ab822e8f27
Edgar Garcia Arrieta <edgarcia@grainchain.io>
2022-12-26 01:47:12 -0600

    add aditional information menu bar

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 import { TranslateService } from '@ngx-translate/core';
+import { ILoginInfoModule } from '../../models/login-info.model';
 import { CONSTANTS } from '../../utils/constants';
 
 @Component({
@@ -9,15 +12,20 @@ import { CONSTANTS } from '../../utils/constants';
 })
 export class MenuBarComponent implements OnInit {
 
-  @Input() user: string = "";
+  @Input() user: string = '';
+  @Input() additionalInfo: ILoginInfoModule = {};
   public nick: string = '';
 
-  constructor(public translate: TranslateService) {
+  constructor(
+    public translate: TranslateService,
+    public auth: AuthService,
+    private _Router: Router) {
     translate.addLangs(CONSTANTS.LANGS_LIST);
     translate.setDefaultLang(CONSTANTS.DEFAULT_LANG);
   }
+
   ngOnInit(): void {
-    this._generateNick(this.user)
+    this._generateNick(this.user);
   }
 
   private _generateNick(name: string) {
@@ -25,5 +33,12 @@ export class MenuBarComponent implements OnInit {
     const [firstName, secondName] = nameFull;
     this.nick = secondName ? `${firstName.charAt(0)}${secondName.charAt(0)}` : `${firstName.charAt(0)}`
   }
+
+  public closeSession() {
+    this.auth.logout();
+    this._Router.navigate(['/login'])
+  }
+
+
 
 }
